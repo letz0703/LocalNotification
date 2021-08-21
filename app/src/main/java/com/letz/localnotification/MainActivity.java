32c3d4f4d475f95e5028a226458608f215127892
letz0703 <letz0703@gmail.com>
2021-08-21 15:07:28 +0900

    Update MainActivity.java
    periodic notification

--- app/src/main/java/com/letz/localnotification/MainActivity.java
+++ app/src/main/java/com/letz/localnotification/MainActivity.java
@@ -3,11 +3,16 @@ package com.letz.localnotification;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationManagerCompat;
 
+import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -20,14 +25,20 @@ public class MainActivity extends AppCompatActivity
 
         button = findViewById(R.id.button);
 
-        button.setOnClickListener(
-                v -> {
-                    counter ++;
-                    button.setText("" + counter);
-                    if (counter == 5){
-                    }
-                }
-        );
+        Calendar calendar = Calendar.getInstance();
+        // set time when the notification will be sent
+        calendar.set(Calendar.HOUR_OF_DAY,2);
+        calendar.set(Calendar.MINUTE,23);
+        calendar.set(Calendar.SECOND,0);
+
+        //operate receiver // make receiver class "Notification_Receiver" to work
+        Intent iStartReceiver = new Intent(getApplicationContext(), Notification_Receiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 100
+        ,iStartReceiver, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis()
+        , AlarmManager.INTERVAL_DAY, pendingIntent);
     }
 }
 
