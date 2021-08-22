0066c89881db32232e940e551f0739b7e101e81a
letz0703 <letz0703@gmail.com>
2021-08-22 10:38:35 +0900

    notification works
    5 times' click on button

--- app/src/main/java/com/letz/localnotification/MainActivity.java
+++ app/src/main/java/com/letz/localnotification/MainActivity.java
@@ -1,5 +1,6 @@
 package com.letz.localnotification;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationManagerCompat;
 
@@ -9,6 +10,7 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -17,6 +19,8 @@ import java.util.Calendar;
 public class MainActivity extends AppCompatActivity
 {
     Button button;
+    public final String CHANNEL_ID = "1";
+    int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,20 +29,31 @@ public class MainActivity extends AppCompatActivity
 
         button = findViewById(R.id.button);
 
-        Calendar calendar = Calendar.getInstance();
-        // set time when the notification will be sent
-        calendar.set(Calendar.HOUR_OF_DAY,2);
-        calendar.set(Calendar.MINUTE,23);
-        calendar.set(Calendar.SECOND,0);
+        button.setOnClickListener(v -> {
+            counter++;
+            button.setText("" + counter);
+            if (counter == 5) {
+                startNotification();
+            }
+        });
+    }
 
-        //operate receiver // make receiver class "Notification_Receiver" to work
-        Intent iStartReceiver = new Intent(getApplicationContext(), Notification_Receiver.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 100
-        ,iStartReceiver, PendingIntent.FLAG_UPDATE_CURRENT);
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void startNotification() {
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "1"
+                , NotificationManager.IMPORTANCE_DEFAULT);
 
-        AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis()
-        , AlarmManager.INTERVAL_DAY, pendingIntent);
+        NotificationManager manager = (NotificationManager) MainActivity.this.getSystemService(MainActivity.this.NOTIFICATION_SERVICE);
+        manager.createNotificationChannel(channel);
+        //customize it
+
+        Notification.Builder builder = new Notification.Builder(MainActivity.this, CHANNEL_ID);
+        builder.setSmallIcon(R.drawable.ic_baseline_add_alert_24).setContentTitle("Title").setContentText("Notification Text")
+                .setPriority(Notification.PRIORITY_DEFAULT);
+
+        //show to user
+        NotificationManagerCompat compat = NotificationManagerCompat.from(MainActivity.this);
+        compat.notify(1, builder.build());
     }
 }
 
